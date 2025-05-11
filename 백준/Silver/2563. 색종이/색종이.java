4eafea40c0b8c072bfbfa50c0a83d01939ac7d9f
이민욱 <56884540+zkvn99@users.noreply.github.com>
2025-05-11 18:23:36 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean[][] paper = new boolean[100][100];
+        int n = Integer.parseInt(br.readLine());
+        int result = 0;
+        
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    paper[j][k] = true;
+                }
+            }
+        }
+        
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (paper[i][j]) {
+                    result++;
+                }
+            }
+        }
+        
+        System.out.print(result);
+    }
+}
