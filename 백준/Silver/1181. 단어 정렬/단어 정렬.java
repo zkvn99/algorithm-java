17a40d4bbca69b1cec8948b1e30db94e441f218b
이민욱 <56884540+zkvn99@users.noreply.github.com>
2025-05-27 21:23:04 +0900

    [Silver V] Title: 단어 정렬, Time: 316 ms, Memory: 24636 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+
+        List<String> wordList = new ArrayList<>(set);
+        wordList.sort((a, b) -> {
+            if (a.length() == b.length()) {
+                return a.compareTo(b);
+            } else {
+                return a.length() - b.length();
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (String word : wordList) {
+            sb.append(word).append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+}
