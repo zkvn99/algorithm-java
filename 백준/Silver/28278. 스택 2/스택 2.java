d539b94c7e2450bf2aa53dc801c52f32c9f73695
이민욱 <56884540+zkvn99@users.noreply.github.com>
2025-07-29 18:43:48 +0900

    [Silver IV] Title: 스택 2, Time: 904 ms, Memory: 308580 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -1,72 +1,39 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+
         int N = Integer.parseInt(br.readLine());
-        ArrayStack arrayStack = new ArrayStack(N);
-        
+
         for (int i = 0; i < N; i++) {
             String line = br.readLine();
+
             if (line.startsWith("1")) {
                 int num = Integer.parseInt(line.split(" ")[1]);
-                arrayStack.push(num);
+                stack.push(num);
             } else {
                 int command = Integer.parseInt(line);
                 switch (command) {
-                    case 2: arrayStack.pop(); break;
-                    case 3: arrayStack.size(); break;
-                    case 4: arrayStack.empty(); break;
-                    case 5: arrayStack.peek(); break;
+                    case 2:
+                        sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n");
+                        break;
+                    case 3:
+                        sb.append(stack.size()).append("\n");
+                        break;
+                    case 4:
+                        sb.append(stack.isEmpty() ? 1 : 0).append("\n");
+                        break;
+                    case 5:
+                        sb.append(stack.isEmpty() ? -1 : stack.peek()).append("\n");
+                        break;
                 }
             }
         }
-    } 
-}
 
-class ArrayStack {
-    int top;
-    int size;
-    int[] stack;
-    public ArrayStack(int size) {
-        this.size = size;
-        stack = new int[size];
-        top = -1;
-    }
-    
-    public void push(int number) {
-        stack[++top] = number;
-    }
-    
-    public void pop() {
-        if(top > -1) {
-            int pop = stack[top];
-            System.out.println(stack[top--]);
-        } else {
-            System.out.println(-1);
-        }
-    }
-    
-    public void peek() {
-        if(top > -1) {
-            System.out.println(stack[top]);
-        } else {
-            System.out.println(-1);
-        }
-    }
-    
-    public void empty() {
-        if(top == -1) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
-        }
-    }
-    
-    public void size() {
-        System.out.println(top + 1);
+        System.out.print(sb);
     }
 }
-
-
