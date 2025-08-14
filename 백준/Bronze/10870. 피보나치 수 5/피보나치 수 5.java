57408a120f1434ff08c34f9680d40dec21e2ba93
이민욱 <56884540+zkvn99@users.noreply.github.com>
2025-08-14 12:25:33 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 104 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        System.out.println(fibonacci(n));
+    }
+    
+    static int fibonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }       
+}
