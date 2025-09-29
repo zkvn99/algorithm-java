e6a86756911edf8752e3a66486d7708f7f312ebb
이민욱 <56884540+zkvn99@users.noreply.github.com>
2025-09-29 12:48:32 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -6,23 +6,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[] stair = new int[N + 1];
-        int[][] dp = new int[N + 1][3];
+        int[] dp = new int[N + 1];
         for (int i = 1; i <= N; i++) {
             stair[i] = Integer.parseInt(br.readLine());
         }
         
-        dp[1][1] = stair[1];
+        dp[1] = stair[1];
+        if (N >= 2) dp[2] = dp[1] + stair[2];
         
-        if (N >= 2) {
-            dp[2][1] = stair[1] + stair[2];
-            dp[2][2] = stair[2];
-        }
-
         for (int i = 3; i <= N; i++) {
-            dp[i][1] = dp[i - 1][2] + stair[i];
-            dp[i][2] = Math.max(dp[i - 2][1], dp[i - 2][2]) + stair[i];
+            dp[i] = Math.max(dp[i-3] + stair[i-1], dp[i-2]) + stair[i];
         }
 
-        System.out.println(Math.max(dp[N][1], dp[N][2]));
+        System.out.println(dp[N]);
     }
 }
