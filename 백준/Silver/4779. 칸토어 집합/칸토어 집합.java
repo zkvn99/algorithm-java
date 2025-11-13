4ac2d85207117e6996a2241e6b4255edc928e314
이민욱 <56884540+zkvn99@users.noreply.github.com>
2025-11-13 11:26:52 +0900

    [Silver III] Title: 칸토어 집합, Time: 196 ms, Memory: 19092 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -1,30 +1,37 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+
+    static char[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s;
-        while ((s = br.readLine()) != null) {
-            int N = Integer.parseInt(s);
-            int len = 1;
-            for (int i = 0; i < N; i++) len *= 3;
 
-            char[] arr = new char[len];
+        while ((s = br.readLine()) != null) {
+            int n = Integer.parseInt(s);
+
+            int len = (int) Math.pow(3, n);
+            arr = new char[len];
             Arrays.fill(arr, '-');
 
-            carve(arr, 0, len);
+            cantor(0, len);
 
             System.out.println(new String(arr));
         }
     }
-    
-    static void carve(char[] arr, int l, int r) {
-        int size = r - l;
-        if (size == 1) return;
-        int third = size / 3;
-        for (int i = l + third; i < l + 2 * third; i++) arr[i] = ' ';
-        carve(arr, l, l + third);
-        carve(arr, l + 2 * third, r);
+
+    static void cantor(int start, int size) {
+        if (size < 3) return;
+
+        int div = size / 3;
+
+        for (int i = start + div; i < start + div * 2; i++) {
+            arr[i] = ' ';
+        }
+
+        cantor(start, div);
+        cantor(start + div * 2, div);
     }
 }
